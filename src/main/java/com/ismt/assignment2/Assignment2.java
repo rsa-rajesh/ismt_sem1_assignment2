6dd3eac110d328b2fd132f89ba90d8449cf248f0
rajeshadhikari <rajeshcht0@gmail.com>
2025-03-26 09:02:15 +0545

    first commit

---
+++ src/main/java/com/ismt/assignment2/Assignment2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ismt.assignment2;
+
+/**
+ *
+ * @author rajeshadhikari
+ */
+public class Assignment2 {
+
+    public static void main(String[] args) {        
+        SplashScreen ss =    new SplashScreen();
+           ss.loadProgressBar();
+    }
+            
+                 
+
+}
